8b452788f8dff2813136e09c51b69903a4b48ed4
Hassan Gul <wedesignerdxb@gmail.com>
2022-09-30 15:46:31 +0500

    searchform styling updated

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -15,11 +15,11 @@ export default function SearchForm(props) {
   }
 
   return (
-    <div>
-    <div class="bg-black py-4 text-white">
-      <input class="shadow appearance-none border rounded rounded-r-none py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+    <div className="flex flex-col">
+    <div className="bg-black py-4 text-white">
+      <input className="p-3 border border-white text-black"
         onChange={(e) => setQuery(e.target.value)}></input>
-      <button class="rounded rounded-l-none text-sm font-semibold leading-6  py-2 px-3 bg-sky-500 text-white cursor-auto shadow-sm" 
+      <button className="p-3 border border-white" 
         onClick={() => searchBooks(query)}>Search</button>
     </div>
     <SearchResults results={results} />
